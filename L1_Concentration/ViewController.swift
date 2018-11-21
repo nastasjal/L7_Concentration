a7fe3ba943a6192d99dad307d01790c49d21bc9e
nastasjal <nastasjal@list.ru>
2018-11-21 16:39:08 +0300

    Initial Commit

---
+++ L1_Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L1_Concentration
+//
+//  Created by Анастасия Латыш on 21/11/2018.
+//  Copyright © 2018 Анастасия Латыш. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
